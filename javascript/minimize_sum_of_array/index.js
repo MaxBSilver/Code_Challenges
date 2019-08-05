2993bb61d0cbd6f9ebe2ec5887803f81c0f31d71
Max Silver <Maxbsilver@gmail.com>
2019-08-05 09:20:32 -0600

    Revert "add top solutions"
    This reverts commit 82099580d0f89142866324bf55e7a61a6b66b8df.

--- javascript/minimize_sum_of_array/index.js
+++ javascript/minimize_sum_of_array/index.js
@@ -12,19 +12,3 @@ function minSum(arr) {
   // use reduce to sum the array of numbers
   return multipliedArr.reduce((a,b) => a+b,0)
 }
-
-//! top solutions 
-function minSum(arr) {
-  const l = arr.length;
-  const sorted = arr.sort((a, b) => b - a);
-  const max = sorted.slice(0, l/2);
-  const min = sorted.slice(l/2, l).reverse();
-  
-  return max.reduce((sum, el, i) => sum + el * min[i], 0);
-}
-
-function minSum(a) {
-  return function minSum(a) {
-    return a.length && a.shift() * a.pop() + minSum(a) ;
-  } ( a.sort( (v,w) => v-w ) ) ;
-}
