6ae2699fcde6d082e7f27426346f750658777c3d
Max Silver <Maxbsilver@gmail.com>
2019-08-21 09:54:55 -0600

    add regex expression for covfefe-7 problem

--- javascript/covfefe-7/index.js
+++ javascript/covfefe-7/index.js
@@ -1,18 +1,23 @@
-const covfefe = (str) =>{
-  let sentence = str.split(' ');
-  let covfefe = false;
-  sentence = sentence.map(word => {
-  if(word.includes('coverage')) {
-    covfefe = true;
-    return word.replace('coverage', 'covfefe')
-    }
-    else return word;
-  })
+const covfefe = str => {
+	let sentence = str.split(' ');
+	let covfefe = false;
+	sentence = sentence.map(word => {
+		if (word.includes('coverage')) {
+			covfefe = true;
+			return word.replace('coverage', 'covfefe');
+		} else return word;
+	});
+
+	if (covfefe) return sentence.join(' ');
+	else {
+		sentence.push('covfefe');
+		return sentence.join(' ');
+  }
   
- if(covfefe) return sentence.join(' ')
- else {
-   sentence.push('covfefe') 
-   return sentence.join(' ');
- }
-  
+};
+
+
+function covfefe(str) {
+  const res = str.replace(/coverage/g, "covfefe")
+  return res === str ? str + " covfefe" : res
 }
