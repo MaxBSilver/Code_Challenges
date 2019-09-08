e179aefa54c1f25dc4517e7e0b98e59a65629889
Max Silver <Maxbsilver@gmail.com>
2019-09-08 17:52:17 -0400

    Add pairs solutions to pairs_of_gloves-6 using an object map and iteration

--- javascript/CodeWars/6/pairs_of_gloves-6/index.js
+++ javascript/CodeWars/6/pairs_of_gloves-6/index.js
@@ -1,4 +1,14 @@
-function numberOfPairs(gloves)
-{
-  //My hands are freezing
-}
+const numberOfPairs = gloves => {
+  let map = {};
+  let pairs = 0;
+  for(let glove of gloves) {
+    if(!map[glove]) map[glove]=1;
+    else { map[glove]++};
+  }
+  for(let color in map) {
+    let num = Math.floor(map[color]/2);
+    pairs = pairs + num;
+  }
+  return pairs;
+  
+};
