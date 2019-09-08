4251587657b20105641540355d5a79f096513346
Max Silver <Maxbsilver@gmail.com>
2019-09-08 17:23:50 -0400

    add longeest consec of strings

--- javascript/CodeWars/6/consec_strings-6/index.js
+++ javascript/CodeWars/6/consec_strings-6/index.js
@@ -1,5 +1,18 @@
 const longestConsec = (strarr, k) => {
-  
-}
+  // generate subarrays for all possible k values
+  let subs = [];
+  let longest = '';
+  for(let i = 0; i<strarr.length; i++){
+    const subarr = strarr.slice(i, i+k);
+    if(subarr.length === k) subs.push(subarr);
+  }
+  // iterate through and join the subarrays, based on if the length is greatest assign to a greatest length value;
 
-longestConsec(["zone", "abigail", "theta", "form", "libe", "zas"], 2);
+  for(let sub of subs) {
+    const joined = sub.join('');
+    if(joined.length > longest.length) longest = joined;
+  }
+	return longest;
+};
+
+longestConsec([ 'zone', 'abigail', 'theta', 'form', 'libe', 'zas' ], 2);
