8647f4230bb91d87e687c97f5d697611b11ccb88
Max Silver <Maxbsilver@gmail.com>
2019-09-08 17:30:51 -0400

    add gloves problem

--- javascript/CodeWars/4/range-4/index.js
+++ javascript/CodeWars/4/range-4/index.js
@@ -1,3 +1,8 @@
 function solution(list){
   // TODO: complete solution 
+  console.log(list)
  }
+
+//  Test.assertEquals(solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]), "-6,-3-1,3-5,7-11,14,15,17-20")
+
+solution([-6, -3, -2, -1, 0, 1, 3, 4, 5, 7, 8, 9, 10, 11, 14, 15, 17, 18, 19, 20]);
